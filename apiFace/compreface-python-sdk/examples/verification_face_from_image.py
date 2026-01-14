71ea5bcb63480f5bba30f38f68a1155b855a2277
4nddrs <joseandres190503@gmail.com>
2026-01-14 16:24:37 -0400

    Convert CompreFace SDK from submodule to regular directory

---
+++ apiFace/compreface-python-sdk/examples/verification_face_from_image.py
@@ -0,0 +1,43 @@
+"""
+Copyright(c) 2021 the original author or authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https: // www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+or implied. See the License for the specific language governing
+permissions and limitations under the License.
+"""
+
+from compreface.collections.face_collections import FaceCollection
+from compreface import CompreFace
+from compreface.service import RecognitionService
+
+DOMAIN: str = "http://localhost"
+PORT: str = "8000"
+RECOGNITION_API_KEY: str = "85c094c6-e31a-4c00-9573-67b4eed440d6"
+
+compre_face: CompreFace = CompreFace(
+    DOMAIN, PORT, {"limit": 0, "det_prob_threshold": 0.8, "status": "true"}
+)
+
+recognition: RecognitionService = compre_face.init_face_recognition(RECOGNITION_API_KEY)
+
+image_path: str = "C:/Users/Andre/Downloads/w.jpg"
+
+face_collection: FaceCollection = recognition.get_face_collection()
+
+print(face_collection.list())
+
+face: dict = next(
+    item for item in face_collection.list().get("faces") if item["subject"] == "Andres"
+)
+
+image_id = face.get("image_id")
+
+print(face_collection.verify(image_path, image_id))
